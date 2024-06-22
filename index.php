65c2bf02c6f1ce62cda133a53d5b4bd27f79eda5
90541115588 <135871495+90541115588@users.noreply.github.com>
2024-06-22 03:37:43 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- /index.php -->
+<?php include 'includes/header.php'; ?>
+<main>
+<h1>THE FAMOUS SINGER</h1>
+<p>The "GOAT" of punjabi music industry Sonu Sito Wala. Apart from being a comedian, Sonu also writes lyrics for his songs.
+His creativity extends beyond comedy, and he expresses himself through music as well. </p>
+<img src="sonu sito wala.jpeg" alt="SONU SITO WALA">
+</main>
+<?php include 'includes/footer.php'; ?>
